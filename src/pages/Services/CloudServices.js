66984702aca1f609dc87cf4139cdad2b2e40d880
BelugaWhaleSam <sameermohd2k1@gmail.com>
2023-08-29 17:19:46 +0530

    all-service-pages

---
+++ src/pages/Services/CloudServices.js
@@ -0,0 +1,12 @@
+import Hero from "../../components/Services/CloudServices/Hero";
+import Features from "../../components/Services/CloudServices/Features";
+const CloudServices = () => {
+  return (
+    <>
+      <Hero />
+      <Features />
+    </>
+  );
+};
+
+export default CloudServices;
---
+++ src/pages/Services/RND.js
@@ -0,0 +1,12 @@
+import Hero from "../../components/Services/RND/Hero";
+import Features from "../../components/Services/RND/Features";
+const RND = () => {
+  return (
+    <>
+      <Hero />
+      <Features />
+    </>
+  );
+};
+
+export default RND;
